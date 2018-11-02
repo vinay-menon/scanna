2f69d98c8227894615d182a27fe801f84a6bbd3f
vinay-menon <vinaymenon313@gmail.com>
2018-11-02 17:15:53 +0530

    change

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ menuButton.addEventListener("click", function(){
     document.body.classList.toggle("menu-open");
     console.log("click")
 });
+
+var prodBtn = document.querySelector(".dropdown > a");
+var dropdnContent = document.querySelector(".dropdown-content");
+var mainHeader = document.querySelector(".main-header");
+var prodSliderTopMargin = window.getComputedStyle(dropdnContent).height;
+
+prodBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.body.classList.toggle("blah");
+
+    mainHeader.style.marginBottom = (mainHeader.style.marginBottom != prodSliderTopMargin) ? prodSliderTopMargin : 0;
+});
